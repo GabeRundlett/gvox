d65a71bb7d8bbadabb6f6a16cfe93fa7409f1b05
GabeRundlett <gabe@rundlett.com>
2022-11-07 21:07:59 -0700

    Replace magicavoxel loader

--- src/formats/magicavoxel.cpp
+++ src/formats/magicavoxel.cpp
@@ -8,6 +8,9 @@
 #include <vector>
 #include <algorithm>
 
+#define OGT_VOX_IMPLEMENTATION
+#include "ogt_vox.hpp"
+
 #if __linux__
 #define EXPORT
 #elif _WIN32
@@ -17,69 +20,8 @@
 struct MagicavoxelVoxel {
     uint8_t x, y, z, color_index;
 };
-struct ChunkID {
+struct MagicavoxelChunkID {
     uint8_t b0, b1, b2, b3;
-};
-
-static const ChunkID CHUNK_ID_VOX_ = {'V', 'O', 'X', ' '};
-static const ChunkID CHUNK_ID_MAIN = {'M', 'A', 'I', 'N'};
-static const ChunkID CHUNK_ID_SIZE = {'S', 'I', 'Z', 'E'};
-static const ChunkID CHUNK_ID_XYZI = {'X', 'Y', 'Z', 'I'};
-static const ChunkID CHUNK_ID_RGBA = {'R', 'G', 'B', 'A'};
-static const ChunkID CHUNK_ID_nTRN = {'n', 'T', 'R', 'N'};
-static const ChunkID CHUNK_ID_nGRP = {'n', 'G', 'R', 'P'};
-static const ChunkID CHUNK_ID_nSHP = {'n', 'S', 'H', 'P'};
-static const ChunkID CHUNK_ID_IMAP = {'I', 'M', 'A', 'P'};
-static const ChunkID CHUNK_ID_LAYR = {'L', 'A', 'Y', 'R'};
-static const ChunkID CHUNK_ID_MATL = {'M', 'A', 'T', 'L'};
-static const ChunkID CHUNK_ID_MATT = {'M', 'A', 'T', 'T'};
-static const ChunkID CHUNK_ID_rOBJ = {'r', 'O', 'B', 'J'};
-static const ChunkID CHUNK_ID_rCAM = {'r', 'C', 'A', 'M'};
-
-static int parse_int32(uint8_t **buffer_ptr) {
-    int result = *(int *)(*buffer_ptr);
-    *buffer_ptr += sizeof(result);
-    return result;
-}
-static void parse_string(uint8_t **buffer_ptr) {
-    int string_size = *(int *)(*buffer_ptr);
-    *buffer_ptr += sizeof(string_size);
-    *buffer_ptr += string_size;
-}
-static void parse_dict(uint8_t **buffer_ptr) {
-    int entry_n = *(int *)(*buffer_ptr);
-    *buffer_ptr += sizeof(entry_n);
-    for (int entry_i = 0; entry_i < entry_n; ++entry_i) {
-        parse_string(buffer_ptr);
-    }
-}
-
-template <typename T>
-static void write_data(uint8_t **buffer_ptr, T const &data) {
-    *reinterpret_cast<T *>(*buffer_ptr) = data;
-    *buffer_ptr += sizeof(T);
-}
-
-struct Parser {
-    uint8_t *buffer_ptr;
-    uint8_t *buffer_sentinel;
-    std::vector<GVoxSceneNode> nodes;
-    bool got_colors = false;
-    void parse_chunk_SIZE();
-    void parse_chunk_XYZI();
-    void parse_chunk_RGBA();
-    void parse_chunk_nTRN();
-    void parse_chunk_nGRP();
-    void parse_chunk_nSHP();
-    void parse_chunk_IMAP();
-    void parse_chunk_LAYR();
-    void parse_chunk_MATL();
-    void parse_chunk_MATT();
-    void parse_chunk_rOBJ();
-    void parse_chunk_rCAM();
-    int parse_chunk();
-    void fixup_last_node();
-    void parse(GVoxPayload const &payload);
 };
 using Palette = std::array<uint32_t, 256>;
 Palette default_palette = {
@@ -99,182 +41,31 @@ Palette default_palette = {
     0xff663300, 0xff333300, 0xff003300, 0xffff0000, 0xffcc0000, 0xff990000, 0xff660000, 0xff330000, 0xff0000ee, 0xff0000dd, 0xff0000bb, 0xff0000aa, 0xff000088, 0xff000077, 0xff000055, 0xff000044,
     0xff000022, 0xff000011, 0xff00ee00, 0xff00dd00, 0xff00bb00, 0xff00aa00, 0xff008800, 0xff007700, 0xff005500, 0xff004400, 0xff002200, 0xff001100, 0xffee0000, 0xffdd0000, 0xffbb0000, 0xffaa0000,
     0xff880000, 0xff770000, 0xff550000, 0xff440000, 0xff220000, 0xff110000, 0xffeeeeee, 0xffdddddd, 0xffbbbbbb, 0xffaaaaaa, 0xff888888, 0xff777777, 0xff555555, 0xff444444, 0xff222222, 0xff111111};
-void Parser::fixup_last_node() {
-    if (nodes.size() == 0 || got_colors)
-        return;
-    size_t size_x = nodes.back().size_x;
-    size_t size_y = nodes.back().size_y;
-    size_t size_z = nodes.back().size_z;
-    size_t size = size_x * size_y * size_z;
-    for (size_t i = 0; i < size; ++i) {
-        auto &voxel = nodes.back().voxels[i];
-        uint32_t palette_value = default_palette[voxel.id];
-        voxel.color.x = static_cast<float>((palette_value >> 0x00) & 0xff) * 1.0f / 255.0f;
-        voxel.color.y = static_cast<float>((palette_value >> 0x08) & 0xff) * 1.0f / 255.0f;
-        voxel.color.z = static_cast<float>((palette_value >> 0x10) & 0xff) * 1.0f / 255.0f;
-    }
-}
-void Parser::parse_chunk_SIZE() {
-    // check if there was a previous chunk, and if it never had a palette applied
-    fixup_last_node();
-    // create the new chunk
-    int size_x = parse_int32(&buffer_ptr);
-    int size_y = parse_int32(&buffer_ptr);
-    int size_z = parse_int32(&buffer_ptr);
-    nodes.push_back({});
-    got_colors = false;
-    nodes.back().size_x = (size_t)size_x;
-    nodes.back().size_y = (size_t)size_y;
-    nodes.back().size_z = (size_t)size_z;
-}
-void Parser::parse_chunk_XYZI() {
-    int voxel_count = parse_int32(&buffer_ptr);
-    size_t size_x = nodes.back().size_x;
-    size_t size_y = nodes.back().size_y;
-    size_t size_z = nodes.back().size_z;
-    size_t size = size_x * size_y * size_z;
-    nodes.back().voxels = (GVoxVoxel *)malloc(sizeof(GVoxVoxel) * size);
-    memset(nodes.back().voxels, 0, size * sizeof(GVoxVoxel));
-    for (int i = 0; i < voxel_count; ++i) {
-        MagicavoxelVoxel voxel = *(reinterpret_cast<MagicavoxelVoxel *>(buffer_ptr) + i);
-        // assert(voxel.x < size_x);
-        // assert(voxel.y < size_y);
-        // assert(voxel.z < size_z);
-        voxel.x = voxel.x < size_x ? voxel.x : (size_x - 1);
-        voxel.y = voxel.y < size_y ? voxel.y : (size_y - 1);
-        voxel.z = voxel.z < size_z ? voxel.z : (size_z - 1);
-        size_t index = voxel.x + voxel.y * size_x + voxel.z * size_x * size_y;
-        nodes.back().voxels[index] = GVoxVoxel{.id = static_cast<uint32_t>(voxel.color_index)};
-    }
-    // seek past voxel data
-    buffer_ptr += sizeof(uint32_t) * (size_t)voxel_count;
-}
-void Parser::parse_chunk_RGBA() {
-    size_t size_x = nodes.back().size_x;
-    size_t size_y = nodes.back().size_y;
-    size_t size_z = nodes.back().size_z;
-    size_t size = size_x * size_y * size_z;
-    Palette &palette = *reinterpret_cast<Palette *>(buffer_ptr);
-    for (size_t i = 0; i < size; ++i) {
-        auto &voxel = nodes.back().voxels[i];
-        uint32_t palette_value = palette[voxel.id];
-        voxel.color.x = static_cast<float>((palette_value >> 0x00) & 0xff) * 1.0f / 255.0f;
-        voxel.color.y = static_cast<float>((palette_value >> 0x08) & 0xff) * 1.0f / 255.0f;
-        voxel.color.z = static_cast<float>((palette_value >> 0x10) & 0xff) * 1.0f / 255.0f;
-    }
-    buffer_ptr += sizeof(Palette);
-    got_colors = true;
-}
-void Parser::parse_chunk_nTRN() {
-    // int node_id = parse_int32(&buffer_ptr);
-    // parse_dict(&buffer_ptr);
-    // int child_node_id = parse_int32(&buffer_ptr);
-    // int reserved_id = parse_int32(&buffer_ptr);
-    // int layer_id = parse_int32(&buffer_ptr);
-    // int frame_n = parse_int32(&buffer_ptr);
-    // for (int frame_i = 0; frame_i < frame_n; ++frame_i) {
-    //     parse_dict(&buffer_ptr);
-    // }
-}
-void Parser::parse_chunk_nGRP() {
-    // int node_id = parse_int32(&buffer_ptr);
-    // parse_dict(&buffer_ptr);
-    // int child_node_n = parse_int32(&buffer_ptr);
-    // for (int child_i = 0; child_i < child_node_n; ++child_i) {
-    //     int child_id = parse_int32(&buffer_ptr);
-    // }
-}
-void Parser::parse_chunk_nSHP() {
-    // int node_id = parse_int32(&buffer_ptr);
-    // parse_dict(&buffer_ptr);
-    // int child_node_n = parse_int32(&buffer_ptr);
-    // for (int child_i = 0; child_i < child_node_n; ++child_i) {
-    //     int model_id = parse_int32(&buffer_ptr);
-    //     parse_dict(&buffer_ptr);
-    // }
-}
-void Parser::parse_chunk_IMAP() {
-}
-void Parser::parse_chunk_LAYR() {
-    // int node_id = parse_int32(&buffer_ptr);
-    // parse_dict(&buffer_ptr);
-    // int reserved_id = parse_int32(&buffer_ptr);
-}
-void Parser::parse_chunk_MATL() {
-    // int material_id = parse_int32(&buffer_ptr);
-    // parse_dict(&buffer_ptr);
-}
-void Parser::parse_chunk_MATT() {
-}
-void Parser::parse_chunk_rOBJ() {
-    parse_dict(&buffer_ptr);
-}
-void Parser::parse_chunk_rCAM() {
-    // int camera_id = parse_int32(&buffer_ptr);
-    // parse_dict(&buffer_ptr);
-}
-int Parser::parse_chunk() {
-    if (!buffer_ptr)
-        return 1;
-    ChunkID chunk_id = *(ChunkID *)(buffer_ptr);
-    buffer_ptr += sizeof(chunk_id);
-    int self_size = parse_int32(&buffer_ptr);
-    int children_size = parse_int32(&buffer_ptr);
-    auto next_buffer_ptr = buffer_ptr + self_size;
-    if (memcmp(&chunk_id, &CHUNK_ID_MAIN, sizeof(ChunkID)) == 0) {
-        buffer_ptr += self_size;
-        buffer_sentinel = (buffer_ptr) + children_size;
-    } else if (memcmp(&chunk_id, &CHUNK_ID_SIZE, sizeof(ChunkID)) == 0) {
-        parse_chunk_SIZE();
-    } else if (memcmp(&chunk_id, &CHUNK_ID_XYZI, sizeof(ChunkID)) == 0) {
-        parse_chunk_XYZI();
-    } else if (memcmp(&chunk_id, &CHUNK_ID_RGBA, sizeof(ChunkID)) == 0) {
-        parse_chunk_RGBA();
-    } else if (memcmp(&chunk_id, &CHUNK_ID_nTRN, sizeof(ChunkID)) == 0) {
-        parse_chunk_nTRN();
-    } else if (memcmp(&chunk_id, &CHUNK_ID_nGRP, sizeof(ChunkID)) == 0) {
-        parse_chunk_nGRP();
-    } else if (memcmp(&chunk_id, &CHUNK_ID_nSHP, sizeof(ChunkID)) == 0) {
-        parse_chunk_nSHP();
-    } else if (memcmp(&chunk_id, &CHUNK_ID_IMAP, sizeof(ChunkID)) == 0) {
-        parse_chunk_IMAP();
-    } else if (memcmp(&chunk_id, &CHUNK_ID_LAYR, sizeof(ChunkID)) == 0) {
-        parse_chunk_LAYR();
-    } else if (memcmp(&chunk_id, &CHUNK_ID_MATL, sizeof(ChunkID)) == 0) {
-        parse_chunk_MATL();
-    } else if (memcmp(&chunk_id, &CHUNK_ID_MATT, sizeof(ChunkID)) == 0) {
-        parse_chunk_MATT();
-    } else if (memcmp(&chunk_id, &CHUNK_ID_rOBJ, sizeof(ChunkID)) == 0) {
-        parse_chunk_rOBJ();
-    } else if (memcmp(&chunk_id, &CHUNK_ID_rCAM, sizeof(ChunkID)) == 0) {
-        parse_chunk_rCAM();
-    } else {
-        fixup_last_node();
-        return 1;
-    }
-    buffer_ptr = next_buffer_ptr;
-    return 0;
-}
-void Parser::parse(GVoxPayload const &payload) {
-    buffer_ptr = payload.data;
-    buffer_sentinel = payload.data + payload.size;
-    if (memcmp(&CHUNK_ID_VOX_, buffer_ptr, sizeof(CHUNK_ID_VOX_)) != 0)
-        return;
-    buffer_ptr += sizeof(CHUNK_ID_VOX_);
-    int version = *(int *)(buffer_ptr);
-    buffer_ptr += sizeof(version);
-    while (buffer_ptr < buffer_sentinel) {
-        if (parse_chunk() != 0)
-            break;
-    }
-    fixup_last_node();
+static const MagicavoxelChunkID CHUNK_VOX_ = {'V', 'O', 'X', ' '};
+static const MagicavoxelChunkID CHUNK_MAIN = {'M', 'A', 'I', 'N'};
+static const MagicavoxelChunkID CHUNK_SIZE = {'S', 'I', 'Z', 'E'};
+static const MagicavoxelChunkID CHUNK_XYZI = {'X', 'Y', 'Z', 'I'};
+static const MagicavoxelChunkID CHUNK_RGBA = {'R', 'G', 'B', 'A'};
+static const MagicavoxelChunkID CHUNK_nTRN = {'n', 'T', 'R', 'N'};
+static const MagicavoxelChunkID CHUNK_nGRP = {'n', 'G', 'R', 'P'};
+static const MagicavoxelChunkID CHUNK_nSHP = {'n', 'S', 'H', 'P'};
+static const MagicavoxelChunkID CHUNK_IMAP = {'I', 'M', 'A', 'P'};
+static const MagicavoxelChunkID CHUNK_LAYR = {'L', 'A', 'Y', 'R'};
+static const MagicavoxelChunkID CHUNK_MATL = {'M', 'A', 'T', 'L'};
+static const MagicavoxelChunkID CHUNK_MATT = {'M', 'A', 'T', 'T'};
+static const MagicavoxelChunkID CHUNK_rOBJ = {'r', 'O', 'B', 'J'};
+static const MagicavoxelChunkID CHUNK_rCAM = {'r', 'C', 'A', 'M'};
+template <typename T>
+static void write_data(uint8_t **buffer_ptr, T const &data) {
+    *reinterpret_cast<T *>(*buffer_ptr) = data;
+    *buffer_ptr += sizeof(T);
 }
 
 extern "C" {
 GVoxPayload EXPORT gvox_create_payload(GVoxScene scene) {
     GVoxPayload result = {};
     struct ChunkHeader {
-        ChunkID id;
+        MagicavoxelChunkID id;
         int self_size;
         int children_size;
     };
@@ -305,12 +96,12 @@ GVoxPayload EXPORT gvox_create_payload(GVoxScene scene) {
     uint8_t *buffer_ptr = result.data;
     write_data(&buffer_ptr, CHUNK_ID_VOX_);
     write_data(&buffer_ptr, version);
-    write_data(&buffer_ptr, ChunkHeader{.id = CHUNK_ID_MAIN, .self_size = 0, .children_size = static_cast<int>(result.size - size_before_main_children)});
-    write_data(&buffer_ptr, ChunkHeader{.id = CHUNK_ID_SIZE, .self_size = sizeof(int) * 3, .children_size = 0});
+    write_data(&buffer_ptr, ChunkHeader{.id = CHUNK_MAIN, .self_size = 0, .children_size = static_cast<int>(result.size - size_before_main_children)});
+    write_data(&buffer_ptr, ChunkHeader{.id = CHUNK_SIZE, .self_size = sizeof(int) * 3, .children_size = 0});
     write_data(&buffer_ptr, static_cast<int>(scene.nodes[0].size_x));
     write_data(&buffer_ptr, static_cast<int>(scene.nodes[0].size_y));
     write_data(&buffer_ptr, static_cast<int>(scene.nodes[0].size_z));
-    write_data(&buffer_ptr, ChunkHeader{.id = CHUNK_ID_XYZI, .self_size = static_cast<int>(sizeof(int) + sizeof(MagicavoxelVoxel) * voxel_n), .children_size = 0});
+    write_data(&buffer_ptr, ChunkHeader{.id = CHUNK_XYZI, .self_size = static_cast<int>(sizeof(int) + sizeof(MagicavoxelVoxel) * voxel_n), .children_size = 0});
     write_data(&buffer_ptr, static_cast<int>(scene.nodes[0].size_x * scene.nodes[0].size_y * scene.nodes[0].size_z));
     for (size_t zi = 0; zi < scene.nodes[0].size_z; ++zi) {
         for (size_t yi = 0; yi < scene.nodes[0].size_y; ++yi) {
@@ -343,7 +134,7 @@ GVoxPayload EXPORT gvox_create_payload(GVoxScene scene) {
             }
         }
     }
-    write_data(&buffer_ptr, ChunkHeader{.id = CHUNK_ID_RGBA, .self_size = static_cast<int>(sizeof(Palette)), .children_size = 0});
+    write_data(&buffer_ptr, ChunkHeader{.id = CHUNK_RGBA, .self_size = static_cast<int>(sizeof(Palette)), .children_size = 0});
     write_data(&buffer_ptr, default_palette);
     return result;
 }
@@ -354,12 +145,39 @@ void EXPORT gvox_destroy_payload(GVoxPayload payload) {
 
 GVoxScene EXPORT gvox_parse_payload(GVoxPayload payload) {
     GVoxScene result = {};
-    Parser parser;
-    parser.parse(payload);
-    result.node_n = parser.nodes.size();
+    ogt_vox_scene const *scene = ogt_vox_read_scene(payload.data, payload.size);
+    result.node_n = 1;
     result.nodes = new GVoxSceneNode[result.node_n];
-    for (size_t i = 0; i < result.node_n; ++i)
-        result.nodes[i] = parser.nodes[i];
+    result.nodes[0].size_x = scene->models[0]->size_x;
+    result.nodes[0].size_y = scene->models[0]->size_y;
+    result.nodes[0].size_z = scene->models[0]->size_z;
+    auto size = result.nodes[0].size_x * result.nodes[0].size_y * result.nodes[0].size_z;
+    result.nodes[0].voxels = (GVoxVoxel *)malloc(sizeof(GVoxVoxel) * size);
+    for (size_t zi = 0; zi < result.nodes[0].size_z; ++zi) {
+        for (size_t yi = 0; yi < result.nodes[0].size_y; ++yi) {
+            for (size_t xi = 0; xi < result.nodes[0].size_x; ++xi) {
+                size_t i = xi + yi * result.nodes[0].size_x + zi * result.nodes[0].size_x * result.nodes[0].size_y;
+                auto const &voxel = scene->models[0]->voxel_data[i];
+                if (voxel == 0) {
+                    result.nodes[0].voxels[i] = GVoxVoxel{
+                        .color = {},
+                        .id = 0,
+                    };
+                } else {
+                    auto const &color = scene->palette.color[voxel];
+                    result.nodes[0].voxels[i] = GVoxVoxel{
+                        .color = {
+                            static_cast<float>(color.r) * 1.0f / 255.0f,
+                            static_cast<float>(color.g) * 1.0f / 255.0f,
+                            static_cast<float>(color.b) * 1.0f / 255.0f,
+                        },
+                        .id = 1,
+                    };
+                }
+            }
+        }
+    };
+    ogt_vox_destroy_scene(scene);
     return result;
 }
 }
