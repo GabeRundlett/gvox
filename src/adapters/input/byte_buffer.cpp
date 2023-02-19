9bdbd3dd7844436c4e6bf93398e2b24827efe895
GabeRundlett <gabe@rundlett.com>
2023-02-18 17:21:19 -0700

    return to prevent array OOB access

--- src/adapters/input/byte_buffer.cpp
+++ src/adapters/input/byte_buffer.cpp
@@ -37,6 +37,7 @@ extern "C" void gvox_input_adapter_byte_buffer_read(GvoxAdapterContext *ctx, siz
     auto &user_state = *static_cast<ByteBufferInputUserState *>(gvox_adapter_get_user_pointer(ctx));
     if (position + size > user_state.bytes.size()) {
         gvox_adapter_push_error(ctx, GVOX_RESULT_ERROR_INPUT_ADAPTER, "Tried reading past the end of the provided input buffer");
+        return;
     }
     std::copy(user_state.bytes.data() + position, user_state.bytes.data() + position + size, static_cast<uint8_t *>(data));
 }
